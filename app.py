ccf24dda77c09f7b1f38b1386cfe715437f6bdac
Jean-Philippe Corbeil <jeanphilippecorbeil@gmail.com>
2020-06-30 13:01:32 -0400

    Rought functional draft using XGBoost as model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, abort
+import pickle
+from flask import Flask, request, abort, jsonify
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -7,13 +8,50 @@ debug = bool(int(int(os.environ["DEBUG"])))
 
 app = Flask(__name__)
 
+models = None
 
-@app.route("/")
+
+def load_models():
+    """
+    Load dict in global variable with models OneVsRest.
+    :return: None.
+    """
+    global models
+
+    with open("xgboost_model.pickle", "rb") as fp:
+        models = pickle.load(fp)
+
+
+def predict(text):
+    """
+    Predict class dict with key as toxic classes and value in {0,1}.
+    :param text: comment (str).
+    :return: dict.
+    """
+    global models
+
+    if models is None:
+        load_models()
+
+    return {k: int(m.predict([text])[0]) for k, m in models.items()}
+
+
+@app.route("/toxic_comment", methods=["GET"])
 def main_route():
-    return abort(404)
+    """
+    Route that predict whether a comment (a string in text GET param) is toxic among 6 classes in multi-label setting.
+    :return: JSON object in which keys are the 6 classes with {0,1} as values.
+    """
+    if "text" in request.args:
+        text = request.args["text"]
+        labels = predict(text)
+        return jsonify(labels)
+    else:
+        return abort(400)
 
 
 if __name__ == "__main__":
+    load_models()
     app.run(debug=debug,
             host=os.environ["FLASK_HOST"],
             port=int(os.environ["FLASK_PORT"]))
