aae1fc0d957f6056eaeed1ab25b496ffe1015ae2
JP <jeanphilippecorbeil@gmail.com>
2020-07-01 13:42:49 -0400

    Refactoring of some functions

--- transformer_finetune.py
+++ transformer_finetune.py
@@ -6,24 +6,30 @@ Original Colab file is located at
 Source: https://towardsdatascience.com/transformers-for-multilabel-classification-71a1a0daf5e1
 """
 
+import pickle
 import pandas as pd
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, f1_score, accuracy_score
 import torch
 from torch.nn import BCEWithLogitsLoss
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, f1_score, accuracy_score
-import pickle
-from transformers import AdamW, BertTokenizer, BertForSequenceClassification
+from transformers import AdamW, RobertaTokenizer, RobertaForSequenceClassification
 from tqdm import trange
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+MODEL_NAME = 'distilroberta-base'
+N_EPOCHS = 3
+BATCH_SIZE = 32
+MAX_LENGTH = 128
+RDN_NUM = 42
+LEARNING_RATE = 2e-5
+NUM_LABELS = 6
 
 df = pd.read_csv('data/train.csv')
 
 cols = df.columns
 label_cols = list(cols[2:])
-num_labels = len(label_cols)
 
 df = df.sample(frac=1).reset_index(drop=True)
 
@@ -32,146 +38,141 @@ df['one_hot_labels'] = list(df[label_cols].values)
 labels = list(df.one_hot_labels.values)
 comments = list(df.comment_text.values)
 
-max_length = 200
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-encodings = tokenizer.batch_encode_plus(comments, max_length=max_length, truncation=True, pad_to_max_length=True)
-print('tokenizer outputs: ', encodings.keys())
+
+def load_model():
+    tokenizer = RobertaTokenizer.from_pretrained(MODEL_NAME, do_lower_case=True)
+    model = RobertaForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=NUM_LABELS)
+    model.cuda()
+    return tokenizer, model
+
+
+tokenizer, model = load_model()
+
+encodings = tokenizer.batch_encode_plus(comments, max_length=MAX_LENGTH, truncation=True, pad_to_max_length=True)
 
 input_ids = encodings['input_ids']
-token_type_ids = encodings['token_type_ids']
 attention_masks = encodings['attention_mask']
-
-label_counts = df.one_hot_labels.astype(str).value_counts()
-one_freq = label_counts[label_counts == 1].keys()
-one_freq_idxs = sorted(list(df[df.one_hot_labels.astype(str).isin(one_freq)].index), reverse=True)
-print('df label indices with only one instance: ', one_freq_idxs)
-
-one_freq_input_ids = [input_ids.pop(i) for i in one_freq_idxs]
-one_freq_token_types = [token_type_ids.pop(i) for i in one_freq_idxs]
-one_freq_attention_masks = [attention_masks.pop(i) for i in one_freq_idxs]
-one_freq_labels = [labels.pop(i) for i in one_freq_idxs]
 
 train_inputs, validation_inputs, \
 train_labels, validation_labels, \
-train_token_types, validation_token_types, \
-train_masks, validation_masks = train_test_split(input_ids, labels, token_type_ids,attention_masks,
-                                                 random_state=2020, test_size=0.10, stratify=labels)
+train_masks, validation_masks = train_test_split(input_ids, labels, attention_masks,
+                                                 random_state=RDN_NUM, test_size=0.10, stratify=labels)
 
-# Add one frequency data to train data
-train_inputs.extend(one_freq_input_ids)
-train_labels.extend(one_freq_labels)
-train_masks.extend(one_freq_attention_masks)
-train_token_types.extend(one_freq_token_types)
 
-# Convert all of our data into torch tensors, the required datatype for our model
-train_inputs = torch.tensor(train_inputs)
-train_labels = torch.tensor(train_labels)
-train_masks = torch.tensor(train_masks)
-train_token_types = torch.tensor(train_token_types)
+def process_training_data(train_inputs, train_labels, train_masks):
+    train_inputs = torch.tensor(train_inputs)
+    train_labels = torch.tensor(train_labels)
+    train_masks = torch.tensor(train_masks)
 
-validation_inputs = torch.tensor(validation_inputs)
-validation_labels = torch.tensor(validation_labels)
-validation_masks = torch.tensor(validation_masks)
-validation_token_types = torch.tensor(validation_token_types)
+    train_data = TensorDataset(train_inputs, train_masks, train_labels)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=BATCH_SIZE)
 
-batch_size = 32
+    torch.save(train_dataloader, 'data/train_data_loader')
 
-train_data = TensorDataset(train_inputs, train_masks, train_labels, train_token_types)
-train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+    return train_dataloader
 
-validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels, validation_token_types)
-validation_sampler = SequentialSampler(validation_data)
-validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
 
-torch.save(validation_dataloader,'validation_data_loader')
-torch.save(train_dataloader,'train_data_loader')
+def process_validation_data(validation_inputs, validation_labels, validation_masks):
+    validation_inputs = torch.tensor(validation_inputs)
+    validation_labels = torch.tensor(validation_labels)
+    validation_masks = torch.tensor(validation_masks)
 
-# Load model, the pretrained model will include a single linear classification layer on top for classification. 
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=num_labels)
-model.cuda()
+    validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+    validation_sampler = SequentialSampler(validation_data)
+    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=BATCH_SIZE)
 
-# setting custom optimization parameters. You may implement a scheduler here as well.
-param_optimizer = list(model.named_parameters())
-no_decay = ['bias', 'gamma', 'beta']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-     'weight_decay_rate': 0.01},
-    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-     'weight_decay_rate': 0.0}
-]
+    torch.save(validation_dataloader, 'data/validation_data_loader')
 
-optimizer = AdamW(optimizer_grouped_parameters, lr=2e-5, correct_bias=True)
+    return validation_dataloader
 
-train_loss_set = []
-epochs = 3
 
-for _ in trange(epochs, desc="Epoch"):
-  model.train()
+train_dataloader = process_training_data(train_inputs, train_labels, train_masks)
+validation_dataloader = process_validation_data(validation_inputs, validation_labels, validation_masks)
 
-  tr_loss = 0 #running loss
-  nb_tr_examples, nb_tr_steps = 0, 0
 
-  for step, batch in enumerate(train_dataloader):
-    # Add batch to GPU
-    batch = tuple(t.to(device) for t in batch)
-    # Unpack the inputs from our dataloader
-    b_input_ids, b_input_mask, b_labels, b_token_types = batch
-    # Clear out the gradients (by default they accumulate)
-    optimizer.zero_grad()
+def config_optimizer():
+    # setting custom optimization parameters. You may implement a scheduler here as well.
+    param_optimizer = list(model.named_parameters())
 
-    # Forward pass for multilabel classification
-    outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
-    logits = outputs[0]
-    loss_func = BCEWithLogitsLoss() 
-    loss = loss_func(logits.view(-1, num_labels), b_labels.type_as(logits).view(-1, num_labels))
-    train_loss_set.append(loss.item())
+    no_decay = ['bias', 'gamma', 'beta']
 
-    loss.backward()
-    optimizer.step()
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.0}
+    ]
 
-    tr_loss += loss.item()
-    nb_tr_examples += b_input_ids.size(0)
-    nb_tr_steps += 1
+    return AdamW(optimizer_grouped_parameters, lr=LEARNING_RATE, correct_bias=True)
 
-  print("Train loss: {}".format(tr_loss/nb_tr_steps))
 
-  model.eval()
+def train(train_dataloader):
+    optimizer = config_optimizer()
 
-  logit_preds, true_labels, pred_labels, tokenized_texts = [], [], [], []
+    train_loss_set = []
+    for _ in trange(N_EPOCHS, desc="Epoch"):
+        model.train()
 
-  for i, batch in enumerate(validation_dataloader):
-    batch = tuple(t.to(device) for t in batch)
-    b_input_ids, b_input_mask, b_labels, b_token_types = batch
-    with torch.no_grad():
-      outs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
-      b_logit_pred = outs[0]
-      pred_label = torch.sigmoid(b_logit_pred)
+        tr_loss = 0
+        nb_tr_examples, nb_tr_steps = 0, 0
 
-      b_logit_pred = b_logit_pred.detach().cpu().numpy()
-      pred_label = pred_label.to('cpu').numpy()
-      b_labels = b_labels.to('cpu').numpy()
+        for step, batch in enumerate(train_dataloader):
 
-    tokenized_texts.append(b_input_ids)
-    logit_preds.append(b_logit_pred)
-    true_labels.append(b_labels)
-    pred_labels.append(pred_label)
+            batch = tuple(t.to(DEVICE) for t in batch)
 
-  # Flatten outputs
-  pred_labels = [item for sublist in pred_labels for item in sublist]
-  true_labels = [item for sublist in true_labels for item in sublist]
+            input_ids, input_mask, labels = batch
 
-  # Calculate Accuracy
-  threshold = 0.50
-  pred_bools = [pl > threshold for pl in pred_labels]
-  true_bools = [tl == 1 for tl in true_labels]
-  val_f1_accuracy = f1_score(true_bools, pred_bools, average='micro')
-  val_flat_accuracy = accuracy_score(true_bools, pred_bools)
+            optimizer.zero_grad()
 
-  print('F1 Validation Accuracy: ', val_f1_accuracy)
-  print('Flat Validation Accuracy: ', val_flat_accuracy)
+            # Forward pass for multilabel classification
+            outputs = model(input_ids, token_type_ids=None, attention_mask=input_mask)
+            logits = outputs[0]
+            loss_func = BCEWithLogitsLoss()
+            loss = loss_func(logits.view(-1, NUM_LABELS), labels.type_as(logits).view(-1, NUM_LABELS))
+            train_loss_set.append(loss.item())
 
-torch.save(model.state_dict(), 'bert_model_toxic')
+            loss.backward()
+            optimizer.step()
+
+            tr_loss += loss.item()
+            nb_tr_examples += input_ids.size(0)
+            nb_tr_steps += 1
+
+        print("Train loss: {}".format(tr_loss/nb_tr_steps))
+
+        model.eval()
+
+        logit_preds, true_labels, pred_labels = [], [], []
+
+        for i, batch in enumerate(validation_dataloader):
+
+            batch = tuple(t.to(DEVICE) for t in batch)
+
+            input_ids, input_mask, labels = batch
+
+            with torch.no_grad():
+                outputs = model(input_ids, token_type_ids=None, attention_mask=input_mask)
+                pred_label = torch.sigmoid(outputs[0])
+
+            logit_preds.append(outputs[0].detach().cpu().numpy())
+            true_labels.append(labels.to('cpu').numpy())
+            pred_labels.append(pred_label.to('cpu').numpy())
+
+        # Flatten outputs
+        pred_labels = [item for sublist in pred_labels for item in sublist]
+        true_labels = [item for sublist in true_labels for item in sublist]
+
+        # Calculate Accuracy
+        threshold = 0.50
+        pred_bools = [pl > threshold for pl in pred_labels]
+        true_bools = [tl == 1 for tl in true_labels]
+        val_f1_accuracy = f1_score(true_bools, pred_bools, average='macro')
+
+        print('Macro F1 on validation: ', val_f1_accuracy)
+
+    torch.save(model.state_dict(), 'model')
+
 
 test_df = pd.read_csv('data/test.csv')
 test_labels_df = pd.read_csv('data/test_labels.csv')
@@ -184,22 +185,20 @@ test_df['one_hot_labels'] = list(test_df[test_label_cols].values)
 test_labels = list(test_df.one_hot_labels.values)
 test_comments = list(test_df.comment_text.values)
 
-test_encodings = tokenizer.batch_encode_plus(test_comments, max_length=max_length, truncation=True, pad_to_max_length=True)
+test_encodings = tokenizer.batch_encode_plus(test_comments, max_length=MAX_LENGTH, truncation=True, pad_to_max_length=True)
 test_input_ids = test_encodings['input_ids']
-test_token_type_ids = test_encodings['token_type_ids']
 test_attention_masks = test_encodings['attention_mask']
 
 # Make tensors out of data
 test_inputs = torch.tensor(test_input_ids)
 test_labels = torch.tensor(test_labels)
 test_masks = torch.tensor(test_attention_masks)
-test_token_types = torch.tensor(test_token_type_ids)
 # Create test dataloader
-test_data = TensorDataset(test_inputs, test_masks, test_labels, test_token_types)
+test_data = TensorDataset(test_inputs, test_masks, test_labels)
 test_sampler = SequentialSampler(test_data)
-test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=BATCH_SIZE)
 # Save test dataloader
-torch.save(test_dataloader, 'test_data_loader')
+torch.save(test_dataloader, 'data/test_data_loader')
 
 # Test
 
@@ -207,27 +206,27 @@ torch.save(test_dataloader, 'test_data_loader')
 model.eval()
 
 #track variables
-logit_preds,true_labels,pred_labels,tokenized_texts = [],[],[],[]
+logit_preds, true_labels, pred_labels, tokenized_texts = [], [], [], []
 
 # Predict
 for i, batch in enumerate(test_dataloader):
-  batch = tuple(t.to(device) for t in batch)
-  # Unpack the inputs from our dataloader
-  b_input_ids, b_input_mask, b_labels, b_token_types = batch
-  with torch.no_grad():
-    # Forward pass
-    outs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
-    b_logit_pred = outs[0]
-    pred_label = torch.sigmoid(b_logit_pred)
+    batch = tuple(t.to(DEVICE) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    with torch.no_grad():
+        # Forward pass
+        outs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+        b_logit_pred = outs[0]
+        pred_label = torch.sigmoid(b_logit_pred)
 
-    b_logit_pred = b_logit_pred.detach().cpu().numpy()
-    pred_label = pred_label.to('cpu').numpy()
-    b_labels = b_labels.to('cpu').numpy()
+        b_logit_pred = b_logit_pred.detach().cpu().numpy()
+        pred_label = pred_label.to('cpu').numpy()
+        b_labels = b_labels.to('cpu').numpy()
 
-  tokenized_texts.append(b_input_ids)
-  logit_preds.append(b_logit_pred)
-  true_labels.append(b_labels)
-  pred_labels.append(pred_label)
+    tokenized_texts.append(b_input_ids)
+    logit_preds.append(b_logit_pred)
+    true_labels.append(b_labels)
+    pred_labels.append(pred_label)
 
 # Flatten outputs
 tokenized_texts = [item for sublist in tokenized_texts for item in sublist]
@@ -244,72 +243,3 @@ print('Test Flat Accuracy: ', accuracy_score(true_bools, pred_bools),'\n')
 clf_report = classification_report(true_bools,pred_bools,target_names=test_label_cols)
 pickle.dump(clf_report, open('classification_report.txt','wb')) #save report
 print(clf_report)
-
-"""## Output Dataframe"""
-
-idx2label = dict(zip(range(6),label_cols))
-print(idx2label)
-
-# Getting indices of where boolean one hot vector true_bools is True so we can use idx2label to gather label names
-true_label_idxs, pred_label_idxs=[],[]
-for vals in true_bools:
-  true_label_idxs.append(np.where(vals)[0].flatten().tolist())
-for vals in pred_bools:
-  pred_label_idxs.append(np.where(vals)[0].flatten().tolist())
-
-# Gathering vectors of label names using idx2label
-true_label_texts, pred_label_texts = [], []
-for vals in true_label_idxs:
-  if vals:
-    true_label_texts.append([idx2label[val] for val in vals])
-  else:
-    true_label_texts.append(vals)
-
-for vals in pred_label_idxs:
-  if vals:
-    pred_label_texts.append([idx2label[val] for val in vals])
-  else:
-    pred_label_texts.append(vals)
-
-# Decoding input ids to comment text
-comment_texts = [tokenizer.decode(text, skip_special_tokens=True, clean_up_tokenization_spaces=False) for text in tokenized_texts]
-
-# Converting lists to df
-comparisons_df = pd.DataFrame({'comment_text': comment_texts, 'true_labels': true_label_texts, 'pred_labels':pred_label_texts})
-comparisons_df.to_csv('comparisons.csv')
-comparisons_df.head()
-
-macro_thresholds = np.array(range(1, 10))/10
-
-f1_results, flat_acc_results = [], []
-for th in macro_thresholds:
-  pred_bools = [pl>th for pl in pred_labels]
-  test_f1_accuracy = f1_score(true_bools,pred_bools,average='micro')
-  test_flat_accuracy = accuracy_score(true_bools, pred_bools)
-  f1_results.append(test_f1_accuracy)
-  flat_acc_results.append(test_flat_accuracy)
-
-best_macro_th = macro_thresholds[np.argmax(f1_results)]
-
-micro_thresholds = (np.array(range(10))/100)+best_macro_th
-
-f1_results, flat_acc_results = [], []
-for th in micro_thresholds:
-  pred_bools = [pl > th for pl in pred_labels]
-  test_f1_accuracy = f1_score(true_bools, pred_bools,average='micro')
-  test_flat_accuracy = accuracy_score(true_bools, pred_bools)
-  f1_results.append(test_f1_accuracy)
-  flat_acc_results.append(test_flat_accuracy)
-
-best_f1_idx = np.argmax(f1_results) #best threshold value
-
-# Printing and saving classification report
-print('Best Threshold: ', micro_thresholds[best_f1_idx])
-print('Test F1 Accuracy: ', f1_results[best_f1_idx])
-print('Test Flat Accuracy: ', flat_acc_results[best_f1_idx], '\n')
-
-best_pred_bools = [pl > micro_thresholds[best_f1_idx] for pl in pred_labels]
-clf_report_optimized = classification_report(true_bools, best_pred_bools, target_names=label_cols)
-pickle.dump(clf_report_optimized, open('classification_report_optimized.txt', 'wb'))
-print(clf_report_optimized)
-
